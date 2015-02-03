a2243f70432067701935f17cf58401a3ec58e08a
Sergey Evseev <evseev@ancort.ru>
2015-02-03 17:27:30 +0300

    Project added

---
+++ FriendlyFormatter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendlyFormatter
+//
+//  Created by Sergey Evseev on 03/02/15.
+//  Copyright (c) 2015 Sergey Evseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FriendlyFormatter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FriendlyFormatter
+//
+//  Created by Sergey Evseev on 03/02/15.
+//  Copyright (c) 2015 Sergey Evseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
