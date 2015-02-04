ffad76f6d91f3451f4d9ac273b50f9758166e337
Sergey Evseev <evseev@ancort.ru>
2015-02-04 15:25:20 +0300

    Test examples added

--- FriendlyFormatter/ViewController.m
+++ FriendlyFormatter/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "NSDate+FriendlyFormatter.h"
 
 @interface ViewController ()
 
@@ -17,11 +18,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self p_runTestExamples];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - private methods
+
+- (void)p_runTestExamples {
+    
+    NSDate *todayDate = [[NSDate alloc] init];
+    NSLog(@"Today: %@", [todayDate friendlyFormat]);
+    
+    NSDate *todayDatePlus4Minutes = [todayDate dateByAddingTimeInterval:60*4];
+    NSLog(@"Today (+4 minutes): %@", [todayDatePlus4Minutes friendlyFormat]);
+    
+    NSDate *yesterdayDate = [todayDate dateByAddingTimeInterval: -86400.0];
+    NSLog(@"Yesterday: %@", [yesterdayDate friendlyFormat]);
+    
+    NSDate *aDayAgo = [todayDate dateByAddingTimeInterval: -2*86400.0];
+    NSLog(@"A day ago: %@", [aDayAgo friendlyFormat]);
+    
+    NSDate *twoDayAgo = [todayDate dateByAddingTimeInterval: -3*86400.0];
+    NSLog(@"Two days ago: %@", [twoDayAgo friendlyFormat]);
 }
 
 @end
